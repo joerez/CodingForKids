ee31046623c79c17e04f92f7242211e1f553cfb8
Joe Rezendes <rezenbiz@gmail.com>
2018-02-05 23:33:36 -0800

    three transitions

--- public/scripts.js
+++ public/scripts.js
@@ -19,13 +19,30 @@ var typed2 = new Typed("#typed", options);
 $(document).ready(function()
 {
 
-$('.rootSec1').fadeOut(9000).css('zIndex', '60', function() {
+$('.rootSec1').fadeOut(9000).css('zIndex', '50', function() {
 
 });
 $('.rootSec2').delay(3000).fadeIn(3000, function(){
   $('.rootSec2').css('display', 'flex');
-  $('.sec2text').delay(100).fadeIn(1000);
+  $('.sec2text').delay(100).fadeIn(1000, function() {
+
+
+      $('.rootSec2').fadeOut(3000, function() {
+
+
+        $('.rootSec3').fadeIn(3000);
+        $('.rootSec3').css('display', 'flex');
+        $('.sec3text').delay(100).fadeIn(4000, function() {
+
+
+        $('.sec3text').addClass('animated pulse infinite')
+
+      })
+
+  });
 
 })
 
+
+})
 })
