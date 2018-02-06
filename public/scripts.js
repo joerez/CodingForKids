bd5e8c6f024fc4bbb0949aba22d407541d7cc907
Joe Rezendes <rezenbiz@gmail.com>
2018-02-05 20:55:41 -0800

    typed.js works

---
+++ public/scripts.js
@@ -0,0 +1,25 @@
+// Can also be included with a regular script tag
+//import Typed from 'typed.js';
+
+
+
+// var typed = new Typed('#typed', {
+//   stringsElement: '#typed-strings'
+// });
+
+
+var options = {
+  strings: ["Hey", "Welcome <br> To", "Coding<br>For<br>Kids"],
+  typeSpeed: 40
+}
+
+var typed2 = new Typed("#typed", options);
+
+
+$(document).ready(function()
+{
+
+$
+
+
+})
