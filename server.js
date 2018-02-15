c8d37e5228e27e272677b3f9159ebf0285ea5d52
Joe Rezendes <rezenbiz@gmail.com>
2018-02-14 20:37:59 -0800

    github register opens link in new tab

--- server.js
+++ server.js
@@ -71,7 +71,8 @@ require('./controllers/reports')(app);
 require('./controllers/teacher.js')(app);
 
 
-//Start Server
-app.listen(process.env.PORT || 3000, () => {
-  console.log('CodingForKids listening on port 3000!');
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`CodingForKids listening on port ${port}!`);
 })
