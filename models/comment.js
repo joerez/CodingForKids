f5b12cefb5249646bbf628fb25cdeb207f89f7df
Joe Rezendes <rezenbiz@gmail.com>
2018-02-15 11:24:52 -0800

    Added comment model

---
+++ models/comment.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+
+const Comment = mongoose.model('Comment', {
+//  title: { type: String, maxlength: 100 },
+//  content: { type: String, maxlength: 1000 },
+  title: String,
+  content: String,
+  reportId: { type: Schema.Types.ObjectId, ref: 'Review' },
+  user: {type : Schema.Types.ObjectId, ref: 'User', required: true},
+  //idpost: [{type: Schema.Types.ObjectId, ref: 'Review'}]
+  assignmentId: { type: Schema.Types.ObjectId, ref: 'GroupPost' }
+//  recaptcha: document.getElementById("g-recaptcha-response").value
+});
+
+module.exports = Comment;
