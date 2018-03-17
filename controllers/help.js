e6114979d9e967f3445b9ff416a18d8fad6d8e09
Joe Rezendes <rezenbiz@gmail.com>
2018-03-17 10:52:40 -0700

    Added routes for help section

---
+++ controllers/help.js
@@ -0,0 +1,68 @@
+const User = require('../models/user');
+const Help = require('../models/help');
+
+var Recaptcha = require('express-recaptcha');
+//import Recaptcha from 'express-recaptcha'
+var recaptcha = new Recaptcha('6LciD0EUAAAAAMSM4b2xRawGOzSD0ke7mlaY-ZpQ', '6LciD0EUAAAAAH4H4CCH0EwKcfbDlQPdMUQe0SFO');
+
+
+
+module.exports = (app) => {
+
+  //POST A HELP PAGE
+  app.post('/help/:id/post', (req, res) => {
+
+
+    let newHelp = new Help(req.body);
+    newHelp.username = req.user.username;
+    newHelp.ip = req.connection.remoteAddress;
+    newHelp.student = req.params.id;
+
+
+    recaptcha.verify(req, function(error, data){
+         if(!error)
+             //success code
+             {
+               newHelp.save((err,report) => {
+                 // ??? Fetch this user
+                 // then add to review to reviews array
+                 // then save
+                 if(err) throw err;
+                 res.redirect('back');
+               })
+             }
+         else
+             //error code
+             {
+               res.redirect('/robot');
+             }
+     });
+
+  })
+
+
+
+  //SHOW HELP PAGE
+  app.get('/help/:id', (req, res) => {
+
+    const findPerson = req.params.id;
+
+
+    let currentUser;
+    if (req.user) {
+        User.findById(req.user._id, (err, user) => {
+      Help.find({student : req.params.id}).then((helps) => {
+        res.render('helps-show', {findPerson, helps, currentUser: user});
+       })
+     })
+     } else {
+       res.redirect('back')
+     }
+ })
+
+
+
+
+
+
+} //modules.exports
