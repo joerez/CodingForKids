973525c5ca1d680f1828dee1500ed842036b8422
Joe Rezendes <rezenbiz@gmail.com>
2018-03-17 10:54:07 -0700

    help model and schema

---
+++ models/help.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+
+const helpSchema = mongoose.Schema({
+//  title: { type: String, maxlength: 140 },
+//  description: { type: String, maxlength: 140 },
+//  player: { type: String, maxlength: 16 },
+  title: String,
+  content: String,
+  imageurl: String,
+  ip: String,
+  teacher : String,
+  student : String,
+  studentId: { type: Schema.Types.ObjectId, ref: 'User' },
+  date : String,
+  comments : [{}]
+//  recaptcha: document.getElementById("g-recaptcha-response").value
+});
+
+const Help = mongoose.model('Help', helpSchema);
+
+module.exports = Help;
